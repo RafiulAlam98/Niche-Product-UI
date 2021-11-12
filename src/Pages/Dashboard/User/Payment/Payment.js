d92a57ced75a0badbd1cb3a82237e50b6c9854b0
unknown <mrafiul.alam7@gmail.com>
2021-11-12 15:04:52 +0600

    review added to the database

--- src/Pages/Dashboard/User/Payment/Payment.js
+++ src/Pages/Dashboard/User/Payment/Payment.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Payment = () => {
      return (
           <div>
-               <h2>this is payment</h2>
+               <h2>Payment System Coming Soon</h2>
           </div>
      );
 };
--- src/Pages/Dashboard/User/Review/Review.js
+++ src/Pages/Dashboard/User/Review/Review.js
@@ -1,9 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import Rating from 'react-rating';
+import useAuth from './../../../../hooks/useAuth/useAuth';
 
 const Review = () => {
+     const {user} = useAuth()
+     const [name,setName] = useState('')
+     const [reviews,setReviews] = useState('')
+     const [rating, setRating] = useState(0)
+
+     const handleName = (e) =>{
+         console.log(e.target.value)
+         setName(e.target.value)
+     }
+     const handleReview = (e) =>{
+          console.log(e.target.value)
+          setReviews(e.target.value)
+     }
+
+     const handleRating = (e) =>{
+          console.log(e.target.value)
+          setRating(e.target.value)
+     }
+
+     const handleSubmit = () =>{
+          const data ={
+                         name: name,
+                         review:reviews,
+                         rating:rating
+                    }
+           console.log(data)
+         
+          fetch('http://localhost:5000/review', {
+               method:'POST',
+               headers:{
+                    'content-type':'application/json'
+               },
+               body:JSON.stringify(data)
+          })
+     }
+
+
+     // console.log(user.email)
      return (
           <div>
-               <h2>this is review</h2>
+               <h2>Review Here</h2>
+
+               <Form className="w-50 mx-auto">
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Reviewer Name</Form.Label>
+                    <Form.Control  onBlur={handleName} type="email" placeholder="name@example.com" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Example textarea</Form.Label>
+                    <Form.Control onBlur={handleReview} as="textarea" rows={5} />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Rating</Form.Label>
+                    <Form.Control  onBlur={handleRating} type="email" placeholder="rating" />
+                    </Form.Group>
+
+                    <Button onClick={handleSubmit}>Submit Review</Button>
+               </Form>
+
+               <Rating
+               initialRating={3}
+               emptySymbol="far fa-star"
+               fullSymbol="fas fa-star"
+               readonly />
           </div>
      );
 };
