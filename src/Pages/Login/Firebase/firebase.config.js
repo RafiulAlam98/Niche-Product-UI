cd52ece17bcd4dc91f52d3119e8a24f5a8b85f50
unknown <mrafiul.alam7@gmail.com>
2021-11-13 19:32:33 +0600

    readme file edited

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
+console.log(process.env)
 const firebaseConfig = {
-     apiKey: "AIzaSyDqnj-ycVjPHKp9eufDamO4BH3-yRMJqE0",
-     authDomain: "niche-product-website-7a068.firebaseapp.com",
-     projectId: "niche-product-website-7a068",
-     storageBucket: "niche-product-website-7a068.appspot.com",
-     messagingSenderId: "685507552505",
-     appId: "1:685507552505:web:74f9e65d8da30dc1f6ae28"
-   };
-
+  apiKey: "AIzaSyDqnj-ycVjPHKp9eufDamO4BH3-yRMJqE0",
+  authDomain: "niche-product-website-7a068.firebaseapp.com",
+  projectId: "niche-product-website-7a068",
+  storageBucket: "niche-product-website-7a068.appspot.com",
+  messagingSenderId: "685507552505",
+  appId: "1:685507552505:web:74f9e65d8da30dc1f6ae28"
+};
    export default firebaseConfig;
